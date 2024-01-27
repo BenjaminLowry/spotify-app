427286ad1ff70638a4c84bbe76d42f40392342be
Benjamin Lowry <benjaminlowry@Benjamins-MacBook-Pro.local>
2024-01-27 15:58:54 -0500

    initial commit

---
+++ spotify-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  spotify-app
+//
+//  Created by Benjamin Lowry on 1/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ spotify-app/MainApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotify_appApp.swift
+//  spotify-app
+//
+//  Created by Benjamin Lowry on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct spotify_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
